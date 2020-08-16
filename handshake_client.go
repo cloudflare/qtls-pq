0cf701a3f70d1b31f6e087d20dba94c50ef854c6
Marten Seemann <martenseemann@gmail.com>
2020-08-16 10:47:37 +0700

    use tls.CertificateRequestInfo instead of redefining it

--- handshake_client.go
+++ handshake_client.go
@@ -909,7 +909,7 @@ func (c *Conn) verifyServerCertificate(certificates [][]byte) error {
 // certificateRequestInfoFromMsg generates a CertificateRequestInfo from a TLS
 // <= 1.2 CertificateRequest, making an effort to fill in missing information.
 func certificateRequestInfoFromMsg(ctx context.Context, vers uint16, certReq *certificateRequestMsg) *CertificateRequestInfo {
-	cri := &CertificateRequestInfo{
+	cri := &certificateRequestInfo{
 		AcceptableCAs: certReq.certificateAuthorities,
 		Version:       vers,
 		ctx:           ctx,
@@ -946,7 +946,7 @@ func certificateRequestInfoFromMsg(ctx context.Context, vers uint16, certReq *ce
 				ECDSAWithP256AndSHA256, ECDSAWithP384AndSHA384, ECDSAWithP521AndSHA512,
 			}
 		}
-		return cri
+		return toCertificateRequestInfo(cri)
 	}
 
 	// Filter the signature schemes based on the certificate types.
@@ -969,7 +969,7 @@ func certificateRequestInfoFromMsg(ctx context.Context, vers uint16, certReq *ce
 		}
 	}
 
-	return cri
+	return toCertificateRequestInfo(cri)
 }
 
 func (c *Conn) getClientCertificate(cri *CertificateRequestInfo) (*Certificate, error) {
