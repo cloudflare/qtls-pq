raulk/qtls

implement additional extensions for the ClientHello message

handshake_messages.go
@@ -93,6 +93,7 @@ type clientHelloMsg struct {
 	pskModes                         []uint8
 	pskIdentities                    []pskIdentity
 	pskBinders                       [][]byte
+	additionalExtensions             []Extension
 }
 
 func (m *clientHelloMsg) marshal() []byte {
@@ -272,6 +273,12 @@ func (m *clientHelloMsg) marshal() []byte {
 					})
 				})
 			}
+			for _, ext := range m.additionalExtensions {
+				b.AddUint16(ext.Type)
+				b.AddUint16LengthPrefixed(func(b *cryptobyte.Builder) {
+					b.AddBytes(ext.Data)
+				})
+			}
 			if len(m.pskIdentities) > 0 { // pre_shared_key must be the last extension
 				// RFC 8446, Section 4.2.11
 				b.AddUint16(extensionPreSharedKey)
@@ -392,14 +399,14 @@ func (m *clientHelloMsg) unmarshal(data []byte) bool {
 	}
 
 	for !extensions.Empty() {
-		var extension uint16
+		var ext uint16
 		var extData cryptobyte.String
-		if !extensions.ReadUint16(&extension) ||
+		if !extensions.ReadUint16(&ext) ||
 			!extensions.ReadUint16LengthPrefixed(&extData) {
 			return false
 		}
 
-		switch extension {
+		switch ext {
 		case extensionServerName:
 			// RFC 6066, Section 3
 			var nameList cryptobyte.String
@@ -586,7 +593,7 @@ func (m *clientHelloMsg) unmarshal(data []byte) bool {
 				m.pskBinders = append(m.pskBinders, binder)
 			}
 		default:
-			// Ignore unknown extensions.
+			m.additionalExtensions = append(m.additionalExtensions, Extension{Type: ext, Data: extData})
 			continue
 		}
 

cloudflare/qtls-pq

handshake_messages.go
@@ -93,6 +93,7 @@ type clientHelloMsg struct {
 	pskModes                         []uint8
 	pskIdentities                    []pskIdentity
 	pskBinders                       [][]byte
+	additionalExtensions             []Extension
 }
 
 func (m *clientHelloMsg) marshal() ([]byte, error) {
@@ -244,6 +245,12 @@ func (m *clientHelloMsg) marshal() ([]byte, error) {
 			exts.AddUint8LengthPrefixed(func(exts *cryptobyte.Builder) {
 				exts.AddBytes(m.pskModes)
 			})
+		})
+	}
+	for _, ext := range m.additionalExtensions {
+		exts.AddUint16(ext.Type)
+		exts.AddUint16LengthPrefixed(func(exts *cryptobyte.Builder) {
+			exts.AddBytes(ext.Data)
 		})
 	}
 	if len(m.pskIdentities) > 0 { // pre_shared_key must be the last extension
@@ -591,7 +598,7 @@ func (m *clientHelloMsg) unmarshal(data []byte) bool {
 				m.pskBinders = append(m.pskBinders, binder)
 			}
 		default:
-			// Ignore unknown extensions.
+			m.additionalExtensions = append(m.additionalExtensions, Extension{Type: extension, Data: extData})
 			continue
 		}
 