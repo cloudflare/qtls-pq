marten-seemann/qtls

add a field to the ConnectionState to tell if 0-RTT was used

record_layer_test.go
@@ -544,6 +544,7 @@ func TestZeroRTTRejection(t *testing.T) {
 			client.Close()
 
 			errChan = make(chan error, 1)
+			connStateChan := make(chan ConnectionState, 1)
 			go func() {
 				serverConf := testConfig.Clone()
 				serverConf.AlternativeRecordLayer = &recordLayer{in: sIn, out: sOut}
@@ -551,6 +552,7 @@ func TestZeroRTTRejection(t *testing.T) {
 				server := Server(&unusedConn{remoteAddr: raddr}, serverConf)
 				defer server.Close()
 				errChan <- server.Handshake()
+				connStateChan <- server.ConnectionState()
 			}()
 
 			clientConf.Enable0RTT = true
@@ -565,7 +567,13 @@ func TestZeroRTTRejection(t *testing.T) {
 				t.Fatalf("second handshake failed %s", err)
 			}
 			if rejected != doReject {
-				t.Fatalf("wrong rejection")
+				t.Fatal("wrong rejection")
+			}
+			if client.ConnectionState().Used0RTT == doReject {
+				t.Fatal("wrong connection state on the client")
+			}
+			if (<-connStateChan).Used0RTT == doReject {
+				t.Fatal("wrong connection state on the server")
 			}
 		})
 	}

cloudflare/qtls-pq

record_layer_test.go
@@ -668,6 +668,7 @@ func TestZeroRTTRejection(t *testing.T) {
 
 			// now dial the second connection
 			errChan = make(chan error, 1)
+			connStateChan := make(chan ConnectionStateWith0RTT, 1)
 			go func() {
 				extraConf := &ExtraConfig{
 					AlternativeRecordLayer: &recordLayer{in: sIn, out: sOut},
@@ -677,6 +678,7 @@ func TestZeroRTTRejection(t *testing.T) {
 				server := Server(&unusedConn{remoteAddr: raddr}, testConfig, extraConf)
 				defer server.Close()
 				errChan <- server.Handshake()
+				connStateChan <- server.ConnectionStateWith0RTT()
 			}()
 
 			extraConf2 := extraConf.Clone()
@@ -692,7 +694,13 @@ func TestZeroRTTRejection(t *testing.T) {
 				t.Fatalf("second handshake failed %s", err)
 			}
 			if rejected != doReject {
-				t.Fatalf("wrong rejection")
+				t.Fatal("wrong rejection")
+			}
+			if client.ConnectionStateWith0RTT().Used0RTT == doReject {
+				t.Fatal("wrong connection state on the client")
+			}
+			if (<-connStateChan).Used0RTT == doReject {
+				t.Fatal("wrong connection state on the server")
 			}
 		})
 	}