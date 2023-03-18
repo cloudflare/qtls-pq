marten-seemann/qtls

add a callback to tell the client about rejection of 0-RTT

handshake_client_tls13.go
@@ -252,6 +252,9 @@ func (hs *clientHandshakeStateTLS13) processHelloRetryRequest() error {
 	}
 
 	hs.hello.earlyData = false // disable 0-RTT
+	if c.config.Rejected0RTT != nil {
+		c.config.Rejected0RTT()
+	}
 
 	hs.transcript.Write(hs.hello.marshal())
 	if _, err := c.writeRecord(recordTypeHandshake, hs.hello.marshal()); err != nil {
@@ -409,6 +412,10 @@ func (hs *clientHandshakeStateTLS13) readServerParameters() error {
 		}
 	}
 	c.clientProtocol = encryptedExtensions.alpnProtocol
+	// Notify the caller if 0-RTT was rejected.
+	if !encryptedExtensions.earlyData && hs.hello.earlyData && c.config.Rejected0RTT != nil {
+		c.config.Rejected0RTT()
+	}
 
 	return nil
 }

cloudflare/qtls-pq

handshake_client_tls13.go
@@ -279,6 +279,9 @@ func (hs *clientHandshakeStateTLS13) processHelloRetryRequest() error {
 		}
 	}
 
+	if hs.hello.earlyData && c.extraConfig != nil && c.extraConfig.Rejected0RTT != nil {
+		c.extraConfig.Rejected0RTT()
+	}
 	hs.hello.earlyData = false // disable 0-RTT
 	if _, err := hs.c.writeHandshakeRecord(hs.hello, hs.transcript); err != nil {
 		return err
@@ -421,6 +424,10 @@ func (hs *clientHandshakeStateTLS13) readServerParameters() error {
 	if !ok {
 		c.sendAlert(alertUnexpectedMessage)
 		return unexpectedMessageError(encryptedExtensions, msg)
+	}
+	// Notify the caller if 0-RTT was rejected.
+	if !encryptedExtensions.earlyData && hs.hello.earlyData && c.extraConfig != nil && c.extraConfig.Rejected0RTT != nil {
+		c.extraConfig.Rejected0RTT()
 	}
 	if hs.c.extraConfig != nil && hs.c.extraConfig.ReceivedExtensions != nil {
 		hs.c.extraConfig.ReceivedExtensions(typeEncryptedExtensions, encryptedExtensions.additionalExtensions)